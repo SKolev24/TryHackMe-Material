d60e13c1bcc1fa8b46aee544afce20f1d5c72eab
Stanislav <StanislavKolev513@gmail.com>
2026-02-09 22:14:33 +0000

    init

---
+++ Hammer THM/BruteForceMFA.py
@@ -0,0 +1,18 @@
+import requests
+
+header = {"Host": "10.65.154.189:1337",
+			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language": "en-US,en;q=0.5",
+			"Content-Type": "application/x-www-form-urlencoded",
+			"Accept-Encoding": "gzip, deflate",
+			"Connection": "keep-alive",
+			"Cookie": "PHPSESSID=c74b49eora2i97po830orf537k",
+			"Upgrade-Insecure-Requests": "1",
+			"Priority": "u=0, i"
+				}
+
+session = requests.Session()
+
+
+
